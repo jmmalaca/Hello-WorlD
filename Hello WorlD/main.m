ac11adb02469cfa8aa4fa86cf212987fe529a8a6
José Miguel Malaca <cartOOn@dhcp-eucpii2-35.ci.uc.pt>
2011-10-25 10:48:34 +0100

    Initial Commit

---
+++ Hello WorlD/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Hello WorlD
+//
+//  Created by José Miguel Malaca on 25/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
