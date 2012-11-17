045bc7c946918f60f13ecc6ce1324ca2ae3494f3
José Miguel Malaca <cartoonfull@gmail.com>
2012-11-17 19:28:47 +0100

    hello
    wOrld...

--- Hello WorlD/main.m
+++ Hello WorlD/main.m
@@ -11,11 +11,13 @@
 int main (int argc, const char * argv[])
 {
 
+    //memory
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 
     // insert code here...
     NSLog(@"Hello, World!");
 
+    //memory
     [pool drain];
     return 0;
 }
